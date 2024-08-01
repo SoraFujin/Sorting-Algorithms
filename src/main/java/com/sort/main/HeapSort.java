7f79630f9ed6052c4761cb96093559f5b11525bb
ahmad diab <ahmaddiab29072002@gmail.com>
2024-08-01 13:24:44 +0300

    First

---
+++ src/main/java/com/sort/main/HeapSort.java
@@ -0,0 +1,12 @@
+package com.sort.main;
+
+public class HeapSort <T extends Comparable<T>> {
+	private T[] data;
+
+	public HeapSort (T[] data) {
+		this.data = data;
+	}
+
+	public void heapSort(){}
+
+}
---
+++ src/main/java/com/sort/main/QuickSort.java
@@ -0,0 +1,12 @@
+package com.sort.main;
+
+public class QuickSort <T extends Comparable <T>> {
+	private T[] data;
+
+	public QuickSort(T[] data) {
+		this.data = data;
+	}
+
+	public void quickSort(){}
+
+}
